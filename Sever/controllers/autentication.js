e31ba77f79f8ff9cbf5a6db944658baaef9abe2a
oncepk <plustor9.ss@gmail.com>
2023-05-19 22:24:22 +0700

    add authentication using node.bcrypt.js
    A library to help you hash passwords.

--- Sever/controllers/autentication.js
+++ Sever/controllers/autentication.js
@@ -1,14 +1,47 @@
 const User = require("../models/user.js");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+// v1
+
+// exports.login = async (req, res) => {
+//   const { email, password } = req.body;
+//   try {
+//     console.log(email, password);
+//     const userDB = await User.findOne({ email: email });
+//     if (password === userDB.password) {
+//       return res.status(200).json(userDB);
+//     }
+//   } catch (err) {
+//     return res.status(400).json({ message: "email or password is wrong" });
+//   }
+// };
+
+
+// v2
 
 exports.login = async (req, res) => {
   const { email, password } = req.body;
   try {
-    console.log(email, password);
-    const userDB = await User.findOne({ email: email });
-    if (password === userDB.password) {
-      return res.status(200).json(userDB);
+    // Find the user by their email
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email or password" });
     }
-  } catch (err) {
-    return res.status(400).json({ message: "email or password is wrong" });
+
+    // Compare the entered password with the hashed password
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    // Return a success response
+    return res.status(200).json({ message: "Login successful" });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
   }
 };
+
+
