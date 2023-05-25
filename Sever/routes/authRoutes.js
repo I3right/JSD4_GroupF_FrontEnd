df0f1e684bfe3168ce064281207d71738ea49de0
Bright <108338051+I3right@users.noreply.github.com>
2023-05-25 21:17:54 +0700

    clear backend

--- Sever/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {login} = require('../controllers/autentication')
-
-router.post("/login", login);
-
-router.get("/test", (req, res) => res.send("works jing jing"));
-
-module.exports = router;
